af2409cbce2f239450c7061b5bb5dfeddaae0784
karamaBattikh <battikh.karama@gmail.com>
2022-07-06 16:44:22 +0200

    test button components

--- src/components/atoms/button/button.test.tsx
+++ src/components/atoms/button/button.test.tsx
@@ -1,13 +1,35 @@
-import {mount  } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 
 import Button from '.';
- 
+
 describe('Button', () => {
   const wrap = mount(<Button label="Button" />);
 
-  test('should render without throwing an error', () => {
+  test('should render Button', () => {
+    expect(wrap);
+    expect(wrap.find('button').length).toEqual(1);
+  });
+
+  test('should render Button with label', () => {
     expect(wrap);
     expect(wrap.find('button').text()).toBe('Button');
+  });
+
+  test('onClick Button', () => {
+    const mockCallBack = jest.fn();
+
+    const button = mount(<Button onClick={mockCallBack} label="Ok!" />);
+    button.find('button').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+
+
+  test('onClick Button disabled', () => {
+    const mockCallBack = jest.fn();
+
+    const button = mount(<Button onClick={mockCallBack} label="Ok!" disabled />);
+    button.find('button').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(0);
   });
 });
--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -5,10 +5,11 @@ import styles from './button.module.scss';
 export interface ButtonProps {
   label?: string;
   onClick?: () => void;
+  disabled?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }: ButtonProps) => (
-  <button className={styles.wrapper} onClick={onClick}>
+const Button: React.FC<ButtonProps> = ({ disabled, label, onClick }: ButtonProps) => (
+  <button className={styles.wrapper} onClick={onClick} disabled={disabled}>
     {label}
   </button>
 );
